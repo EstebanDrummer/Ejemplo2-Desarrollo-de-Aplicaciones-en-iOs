80856e53c59ac4affd74de74a482cb19d3087c0d
EstebanDrummer <yepesk8r@gmail.com>
2013-05-01 20:18:59 -0500

    Initial Commit

---
+++ Ejemplo2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ejemplo2
+//
+//  Created by Esteban Yepes on 1/05/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ejemplo2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ejemplo2
+//
+//  Created by Esteban Yepes on 1/05/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
